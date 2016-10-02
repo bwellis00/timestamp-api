308413cf756c23c9aff3a49a3609b842043b099c
Brandon Ellis <bwellis@gmail.com>
2016-10-02 04:33:17 +0000

    update

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 var express = require('express');
+var moment = require('moment');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:date', function(req, res){
+
+  var date = req.params.date;
+  
+  if(isNaN(date))
+  {
+    date = moment(date, "MMMM D, YYYY");    
+  }
+  else
+  {
+    date = moment(date, "X");  
+  }
+  
+  if(date.isValid())
+  {
+      
+    res.json(
+        {
+        unix: moment(date).format("X"),   
+        natural: moment(date).format("MMMM D, YYYY")    
+        }
+        
+        )
+      
+  }
+  
+  else
+  {
+      
+    res.json(
+        {
+        unix: null, 
+        natural: null
+        }
+        );
+      
+  }
+  
+
+    
+
+  
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.listen(8080);
