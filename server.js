1a8f76586514ec9e1e76b04f5f515e0019f63025
Brandon Ellis <bwellis@gmail.com>
2016-10-03 05:11:03 +0000

    update 3

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var moment = require('moment');
+var path = require('path');
 
 var app = express();
 
 var port = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 
 app.get('/:date', function(req, res){
 
